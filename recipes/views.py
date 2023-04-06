a13ad64a53f746f0ebed08f2a92888dc15dae242
Luiz Santos <lg.motta@hotmail.com>
2023-04-05 21:00:28 -0300

    add category filter to the views

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,27 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.all().order_by("-id")
     return render(
         request,
         "recipes/pages/home.html",
-        context={"recipes": [make_recipe() for _ in range(10)]},
+        context={"recipes": recipes},
+        status=200,
+    )
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by("-id")
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={"recipes": recipes},
         status=200,
     )
 
