d5997624e5acacd0fdb05a7730c5b8aa14f4c5cc
Luiz Santos <lg.motta@hotmail.com>
2023-04-10 21:08:58 -0300

    show recipe from db and it  html preparation

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
@@ -32,11 +32,13 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+
     return render(
         request,
         "recipes/pages/recipe-view.html",
         context={
-            "recipe": make_recipe(),
+            "recipe": recipe,
             "is_detail_page": True,
         },
         status=200,
