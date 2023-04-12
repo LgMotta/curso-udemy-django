fd551bfba195342d13e7b81877bf8a017df31ed6
Luiz Santos <lg.motta@hotmail.com>
2023-04-12 20:58:59 -0300

    refactor test into files in folder test

--- recipes/tests/test_recipes_views.py
+++ recipes/tests/test_recipes_views.py
@@ -4,20 +4,6 @@ from django.urls import resolve, reverse
 from recipes import views
 
 
-class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        home_url = reverse("recipes:home")
-        self.assertEqual(home_url, "/")
-
-    def test_recipe_category_url_is_correct(self):
-        category_url = reverse("recipes:category", kwargs={"category_id": 1})
-        self.assertEqual(category_url, "/recipes/category/1/")
-
-    def test_recipe_detail_url_is_correct(self):
-        recipe_url = reverse("recipes:recipe", kwargs={"id": 1})
-        self.assertEqual(recipe_url, "/recipes/1/")
-
-
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_is_correct(self):
         view = resolve(reverse("recipes:home"))
